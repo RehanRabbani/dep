55fe1585aacee58161189eb5c3ddbfa478fe7bd2
RehanRabbani <rabbanirehan6@gmail.com>
2019-05-27 13:09:55 +0500

    cors

--- index.js
+++ index.js
@@ -9,6 +9,36 @@ require("dotenv").config()
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client","build")));
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+app.use(cors({
+  methods: ['GET','POST','PUT','PATCH'],
+  credentials: true, origin: true,
+}))
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
+  // res.header("Access-Control-Allow-Origin","*");
+  // res.header(
+      
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept , Authorization"  
+  // );
+  // if(req.method === "OPTIONS"){
+  //     res.header('Access-Control-Allow-Methods',"GET,PUT,POST,PATCH,DELETE");
+  //     return res.status(200).json({});
+  // }
+  // next();
+})
+
+
+  app.use(allowCrossDomain);
 
 app.use("/info",router);
 // The "catchall" handler: for any request that doesn't
